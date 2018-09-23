63ac941d8046ef0dd33297a9e05e57fa259ce38c
BuildTools <unconfigured@null.spigotmc.org>
2018-09-22 23:05:03 -0400

    Initial commit

---
+++ app/src/main/java/fenn/christian/customerapp/Billing.java
@@ -0,0 +1,13 @@
+package fenn.christian.customerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Billing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_billing);
+    }
+}
